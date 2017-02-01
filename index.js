97ce739054995e00c338c7f6a35a7812d0073fb5
Michael Donat <michael.donat@me.com>
2017-02-01 11:16:10 +0000

    Added error.reference to log records

--- index.js
+++ index.js
@@ -6,72 +6,73 @@ const safeCycles = bunyan.safeCycles;
 
 // modified from https://github.com/trentm/node-bunyan/blob/master/examples/specific-level-streams.js
 class RecordWriter {
-  constructor(levels, stream) {
-    this.levels = {};
-    levels.forEach((lvl) => {
-      this.levels[bunyan.resolveLevel(lvl)] = true;
-    });
-    this.stream = stream;
-  }
+	constructor(levels, stream) {
+		this.levels = {};
+		levels.forEach((lvl) => {
+			this.levels[bunyan.resolveLevel(lvl)] = true;
+		});
+		this.stream = stream;
+	}
 
-  mapLevelToString(lvl) {
-    return bunyan.nameFromLevel[lvl]
-  }
+	mapLevelToString(lvl) {
+		return bunyan.nameFromLevel[lvl]
+	}
 
-  write(record) {
-    if (this.levels[record.level] !== undefined) {
-      record.severity = this.mapLevelToString(record.level)
-      const str = JSON.stringify(record, safeCycles()) + '\n';
-      this.stream.write(str);
-    }
-  }
+	write(record) {
+		if (this.levels[record.level] !== undefined) {
+			record.severity = this.mapLevelToString(record.level)
+			const str = JSON.stringify(record, safeCycles()) + '\n';
+			this.stream.write(str);
+		}
+	}
 }
 
 function getStack(err) {
-  if (!err.stack) return;
+	if (!err.stack) return;
 
-  const frames = parsetrace(err).object().frames;
+	const frames = parsetrace(err).object().frames;
 
-  return frames.map(f => `${f.function} :: ${f.file}:${f.line}`);
+	return frames.map(f => `${f.function} :: ${f.file}:${f.line}`);
 }
 
 function errSerializer(err) {
-			return {
-              type: err.type,
-              status: err.status,
-              message: err.message,
-              stack: getStack(err),
-              previous: err.previous ? errSerializer(err.previous) : undefined
-			};
-		}
+	return {
+		type: err.type,
+		status: err.status,
+		message: err.message,
+		stack: getStack(err),
+		previous: err.previous ? errSerializer(err.previous) : undefined,
+		reference: err.reference
+	};
+}
 
 
-module.exports = function(config) {
-  const logger = bunyan.createLogger(merge.recursive({
-    serializers: {err: errSerializer, req: bunyan.stdSerializers.req, error: errSerializer},
-    streams: [
-      {
-        type: 'raw',
-        stream: new RecordWriter(
-          [bunyan.ERROR, bunyan.FATAL],
-          process.stderr
-        ),
-        level: bunyan.ERROR
-      },
-      {
-        type: 'raw',
-        stream: new RecordWriter(
-          [bunyan.TRACE, bunyan.INFO, bunyan.DEBUG, bunyan.WARN],
-          process.stdout
-        ),
-        level: bunyan.TRACE
-      }
-    ]
-  },config));
+module.exports = function (config) {
+	const logger = bunyan.createLogger(merge.recursive({
+		serializers: {err: errSerializer, req: bunyan.stdSerializers.req, error: errSerializer},
+		streams: [
+			{
+				type: 'raw',
+				stream: new RecordWriter(
+					[bunyan.ERROR, bunyan.FATAL],
+					process.stderr
+				),
+				level: bunyan.ERROR
+			},
+			{
+				type: 'raw',
+				stream: new RecordWriter(
+					[bunyan.TRACE, bunyan.INFO, bunyan.DEBUG, bunyan.WARN],
+					process.stdout
+				),
+				level: bunyan.TRACE
+			}
+		]
+	}, config));
 
-  if (config.level) {
-    logger.level(config.level)
-  }
+	if (config.level) {
+		logger.level(config.level)
+	}
 
-  return logger;
+	return logger;
 }
